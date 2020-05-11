581a11d6b98c085c13aad82767cfbfdecccea978
fmckee <41274851+McKeeAtx@users.noreply.github.com>
2020-05-11 13:13:37 -0500

    added package-lock.json and more test cases

--- src/boardstate/BoardStateTest.test.js
+++ src/boardstate/BoardStateTest.test.js
@@ -8,8 +8,8 @@ import Queen from "../pieces/queen/Queen";
 import King from "../pieces/king/King";
 import Pawn from "../pieces/pawn/Pawn";
 
-describe('initial state' , function(){
-    it('is as expected for white' , function(){
+describe('initial state' , function() {
+    it('is as expected for white' , function() {
         let boardState = BoardState.initial();
 
         expect(boardState.getData(0, 1).piece).toEqual(new Pawn(Color.WHITE));
@@ -31,7 +31,7 @@ describe('initial state' , function(){
         expect(boardState.getData(7, 0).piece).toEqual(new Rook(Color.WHITE));
     });
 
-    it('is as expected for black' , function(){
+    it('is as expected for black' , function() {
         let boardState = BoardState.initial();
 
         expect(boardState.getData(0, 6).piece).toEqual(new Pawn(Color.BLACK));
@@ -52,4 +52,31 @@ describe('initial state' , function(){
         expect(boardState.getData(6, 7).piece).toEqual(new Knight(Color.BLACK));
         expect(boardState.getData(7, 7).piece).toEqual(new Rook(Color.BLACK));
     });
+});
+
+describe('highlighting' , function(){
+    it('initially no square is highlighted' , function() {
+        let boardState = BoardState.initial();
+        for (let col = 0; col < 8; col++) {
+            for (let row = 0; row < 8; row++) {
+                expect(boardState.getData(7, 7).highlighted).toEqual(false);
+            }
+        }
+    });
+
+    it('a click on an empty square does not result in highlighting' , function() {
+        let boardState = BoardState.initial().onClick(4, 4);
+        expect(boardState.getData(4, 4).highlighted).toEqual(false);
+    });
+
+    it('own pieces are highlighted upon click' , function() {
+        let boardState = BoardState.initial().onClick(0, 0);
+        expect(boardState.getData(0, 0).highlighted).toEqual(true);
+    });
+
+    it('opponent pieces are not highlighted upon click' , function() {
+        let boardState = BoardState.initial().onClick(7, 7);
+        expect(boardState.getData(7, 7).highlighted).toEqual(false);
+    });
+
 });
