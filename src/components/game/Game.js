a051e6d8056f8e522d945c079a2bfc49eae872f4
fmckee <41274851+McKeeAtx@users.noreply.github.com>
2020-05-12 19:57:24 -0500

    inital work on time travel

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -9,35 +9,35 @@ class Game extends React.Component {
         super(props);
         this.state = {
             gameStates: [GameState.initialBoard()],
-            currentState: 0
+            indexOfCurrentState: 0
         };
     }
 
     handleSquareClick(col, row) {
         const lastState = this.state.gameStates[this.state.gameStates.length - 1];
-        const newState = this.state.gameStates.slice();
-        newState.push(lastState.handleSquareClick(col, row));
+        const newStates = this.state.gameStates.slice();
+        newStates.push(lastState.handleSquareClick(col, row));
         this.setState({
-            gameStates: newState,
-            currentState: newState.length - 1
+            gameStates: newStates,
+            indexOfCurrentState: newStates.length - 1
         });
     }
 
     handleMoveClick(index) {
         this.setState({
-            currentState: index
+            indexOfCurrentState: index
         });
     }
 
     render() {
-        const lastState = this.state.gameStates[this.state.gameStates.length - 1];
-        const currentState = this.state.gameStates[this.state.currentState];
+        const currentState = this.state.gameStates[this.state.indexOfCurrentState];
         let moves = [];
         for (let i = 0; i < this.state.gameStates.length; i++) {
             if (this.state.gameStates[i].moves.length != moves.length) {
                 moves.push({
                     title: this.state.gameStates[i].moves[this.state.gameStates[i].moves.length - 1],
-                    index: i
+                    index: i,
+                    btnClass: i === this.state.indexOfCurrentState ? 'highlighted' : ''
                 })
             }
         }
@@ -49,7 +49,7 @@ class Game extends React.Component {
                 <div className="game-info">
                     <div>Moves:</div>
                     <ol>
-                        {moves.map(move => <div key={"move-" + move.index}><button  onClick={() => this.handleMoveClick(move.index)}>{move.title}</button></div>)}
+                        {moves.map(move => <div key={"move-" + move.index}><button className={move.btnClass} onClick={() => this.handleMoveClick(move.index)}>{move.title}</button></div>)}
                     </ol>
                 </div>
 
