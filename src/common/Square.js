c333c6c38dd21c5c14a99a5be539c1e101dc2795
fmckee <41274851+McKeeAtx@users.noreply.github.com>
2020-05-15 00:44:47 -0500

    more work to make GameState immutable

--- src/common/Square.js
+++ src/common/Square.js
@@ -90,7 +90,7 @@ const F8 = new Square('F8', 5, 7);
 const G8 = new Square('G8', 6, 7);
 const H8 = new Square('H8', 7, 7);
 
-export const all = [
+export const all = ([
     A1, A2, A3, A4, A5, A6, A7, A8,
     B1, B2, B3, B4, B5, B6, B7, B8,
     C1, C2, C3, C4, C5, C6, C7, C8,
@@ -99,7 +99,7 @@ export const all = [
     F1, F2, F3, F4, F5, F6, F7, F8,
     G1, G2, G3, G4, G5, G6, G7, G8,
     H1, H2, H3, H4, H5, H6, H7, H8
-].sort(Square.COMPARATOR);
+].sort(Square.COMPARATOR));
 
 export default Square;
 export { 
